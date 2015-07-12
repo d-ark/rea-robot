6ac9dfe29c41020df576c4dc416780bdcdef9cd6
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 20:28:28 +0300

    [GREEN] Added cell validations

--- lib/rea/robot/robot.rb
+++ lib/rea/robot/robot.rb
@@ -2,18 +2,27 @@ module Rea
   module Robot
     class Robot
 
+      def initialize board
+        @board = board
+      end
+
       def position
         cell && direction ? "#{cell.to_s},#{direction.upcase}" : ""
       end
 
       def place cell, direction
+        return if invalid_cell? cell
         @cell = cell
         @direction = direction
       end
 
     private
 
-      attr_reader :cell, :direction
+      attr_reader :cell, :direction, :board
+
+      def invalid_cell? cell
+        !cell.on_board? board
+      end
 
     end
   end

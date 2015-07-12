dc3333bbcf79d957e816c712cfc89bb80779728f
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 19:05:49 +0300

    [GREEN] Implemented board

--- lib/rea/robot/board.rb
+++ lib/rea/robot/board.rb
@@ -1,6 +1,14 @@
 module Rea
   module Robot
     class Board
+      def initialize width, height
+        @width = width
+        @height = height
+      end
+
+      def on_board? coord_x, coord_y
+        (0..@width-1).include?(coord_x) && (0..@height-1).include?(coord_y)
+      end
     end
   end
 end
--- spec/rea/robot/board_spec.rb
+++ spec/rea/robot/board_spec.rb
@@ -12,7 +12,7 @@ describe Rea::Robot::Board do
       inside_samples = [0,0], [1,1], [0,4], [4,4], [2,3]
 
       inside_samples.each do |sample|
-        expect(board.on_board *sample).to be
+        expect(board.on_board? *sample).to be
       end
     end
 
@@ -20,7 +20,7 @@ describe Rea::Robot::Board do
       outside_samples = [-1,0], [-1,-1], [5,-1], [5,1], [-78,3]
 
       outside_samples.each do |sample|
-        expect(board.on_board *sample).not_to be
+        expect(board.on_board? *sample).not_to be
       end
     end
 

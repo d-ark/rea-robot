47a8ba4ab4a9def1f29a88b5dc34ef33d31da09b
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 18:25:59 +0300

    [GREEN] Implemented BoardCell

--- lib/rea/robot/board_cell.rb
+++ lib/rea/robot/board_cell.rb
@@ -1,6 +1,18 @@
 module Rea
   module Robot
     class BoardCell
+      def initialize coord_x, coord_y
+        @coord_x = coord_x
+        @coord_y = coord_y
+      end
+
+      def == other
+        @coord_x == other.coord_x && @coord_y == other.coord_y
+      end
+
+      protected
+
+        attr_reader :coord_x, :coord_y
 
     end
   end

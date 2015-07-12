88f570f1e4f817e10fe13551c6ea05afe49a1bde
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 00:49:43 +0300

    [RED] Added more feature tests

--- spec/features/robot_movements_spec.rb
+++ spec/features/robot_movements_spec.rb
@@ -3,16 +3,33 @@ require "spec_helper"
 describe "Rea Robot" do
   let(:exe) { File.expand_path('../../../bin/rea-robot', __FILE__) }
 
-  describe "moving forward" do
-    let(:cmd) {"#{exe}" }
+  let(:cmd) {"#{exe}" }
 
-    it "does simple moving" do
-      inputs = ["PLACE 0,0,NORTH", "MOVE", "REPORT", "EXIT"]
+  it "does simple moving" do
+    inputs = ["PLACE 0,0,NORTH", "MOVE", "REPORT"]
 
-      process = run_interactive(cmd)
-      inputs.each { |input_line| type input_line }
+    process = run_interactive(cmd)
+    inputs.each { |input_line| type input_line }
+    close_input
 
-      expect(process.output).to include("0,1,NORTH")
-    end
+    expect(process.output).to eq("0,1,NORTH\n")
+  end
+
+  it "does simple rotating" do
+    inputs = ["PLACE 0,0,NORTH", "LEFT", "REPORT", "EXIT"]
+
+    process = run_interactive(cmd)
+    inputs.each { |input_line| type input_line }
+
+    expect(process.output).to eq("0,0,WEST\n")
+  end
+
+  it "does not fail" do
+    inputs = ["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE", "REPORT", "EXIT"]
+
+    process = run_interactive(cmd)
+    inputs.each { |input_line| type input_line }
+
+    expect(process.output).to eq("3,3,NORTH\n")
   end
 end

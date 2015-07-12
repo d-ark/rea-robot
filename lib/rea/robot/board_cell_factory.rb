1c9d27df71f60ad22978f7e4c0ccd53f10e44f31
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 01:45:12 +0300

    [GREEN] Implemented BoardCellFactory

--- lib/rea/robot/board_cell_factory.rb
+++ lib/rea/robot/board_cell_factory.rb
@@ -4,6 +4,11 @@ require "rea/robot/null_board_cell"
 module Rea
   module Robot
     class BoardCellFactory
+      def self.build left, top
+        return NullBoardCell.new unless left.to_i.to_s == left.to_s
+        return NullBoardCell.new unless top.to_i.to_s  == top.to_s
+        BoardCell.new left.to_i, top.to_i
+      end
     end
   end
 end

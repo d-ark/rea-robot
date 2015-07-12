42841d37dd3c7ec314d9430ad29f38926a5843d9
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 20:12:28 +0300

    [GREEN] Implemented place for robot

--- lib/rea/robot/robot.rb
+++ lib/rea/robot/robot.rb
@@ -1,9 +1,20 @@
 module Rea
   module Robot
     class Robot
+
       def position
-        ''
+        cell && direction ? "1,1,#{direction.upcase}" : ""
       end
+
+      def place cell, direction
+        @cell = cell
+        @direction = direction
+      end
+
+    private
+
+      attr_reader :cell, :direction
+
     end
   end
 end

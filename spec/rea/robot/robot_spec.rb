0e545dbfbfad3e47848de3dd6431a940b0d0f6ed
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 02:52:19 +0300

    [RED] Change to strings to Direction-objects (in spec)

--- spec/rea/robot/robot_spec.rb
+++ spec/rea/robot/robot_spec.rb
@@ -4,12 +4,18 @@ require "rea/robot/board_cell"
 require "rea/robot/board"
 
 describe Rea::Robot::Robot do
+  let(:north) { Rea::Robot::Direction.new 'north' }
+  let(:west) { Rea::Robot::Direction.new 'west' }
+  let(:south) { Rea::Robot::Direction.new 'south' }
+  let(:east) { Rea::Robot::Direction.new 'east' }
+  let(:invalid_direction) { Rea::Robot::Direction.new('ololo') }
+
   let(:board) { Rea::Robot::Board.new 5, 5 }
   let(:cell) { Rea::Robot::BoardCell.new 1, 1 }
   let(:robot) { described_class.new board }
   let(:placed_robot) do
     placed_robot = described_class.new(board)
-    placed_robot.place cell, 'north'
+    placed_robot.place cell, north
     placed_robot
   end
 
@@ -23,28 +29,28 @@ describe Rea::Robot::Robot do
     let(:bad_cell) { Rea::Robot::BoardCell.new -1, 78 }
 
     it 'places directed robot to the cell' do
-      robot.place cell, 'west'
+      robot.place cell, west
       expect(robot.position).to eq '1,1,WEST'
     end
 
     it 'changes robots position' do
-      placed_robot.place cell, 'west'
-      expect { placed_robot.place antother_cell, 'north' }.to change { placed_robot.position }
+      placed_robot.place cell, west
+      expect { placed_robot.place antother_cell, north }.to change { placed_robot.position }
         .from('1,1,WEST').to('2,3,NORTH')
     end
 
     it 'cannot place robot outside the board' do
-      robot.place bad_cell, 'west'
+      robot.place bad_cell, west
       expect(robot.position).to eq ''
     end
 
     it 'does not change robots position to outside the board' do
-      placed_robot.place cell, 'west'
-      expect { placed_robot.place bad_cell, 'north' }.not_to change { placed_robot.position }
+      placed_robot.place cell, west
+      expect { placed_robot.place bad_cell, north }.not_to change { placed_robot.position }
     end
 
     it 'cannot place robot with bad direction' do
-      robot.place cell, 'ololo'
+      robot.place cell, invalid_direction
       expect(robot.position).to eq ''
     end
   end
@@ -83,7 +89,7 @@ describe Rea::Robot::Robot do
     end
 
     it 'doesnt move robot outside the board' do
-      robot.place corner_cell, 'south'
+      robot.place corner_cell, south
       expect { robot.move }.not_to change { robot.position }
     end
   end

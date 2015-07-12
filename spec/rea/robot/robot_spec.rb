aca7780f46260db3f88bb46a91b6569d9815b0f6
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 20:05:17 +0300

    Added specs for placing robot

--- spec/rea/robot/robot_spec.rb
+++ spec/rea/robot/robot_spec.rb
@@ -1,11 +1,22 @@
 require "spec_helper"
 require "rea/robot/robot"
+require "rea/robot/board_cell"
 
 describe Rea::Robot::Robot do
   let(:robot) { described_class.new }
 
   it 'creates robot with empty position (not placed)' do
     expect(robot.position).to eq ''
+  end
+
+  describe '#place' do
+    let(:cell) { Rea::Robot::BoardCell.new 1, 1 }
+
+    it 'places directed robot to the cell' do
+      robot.place cell, 'west'
+      expect(robot.position).to eq '1,1,WEST'
+    end
+
   end
 
 end

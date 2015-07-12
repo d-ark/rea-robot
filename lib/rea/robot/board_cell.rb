4d2bc3d1eab8c5c4a378db74a91d376a95a6731b
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 19:50:36 +0300

    [GREEN] Implemented neighbours for cell

--- lib/rea/robot/board_cell.rb
+++ lib/rea/robot/board_cell.rb
@@ -14,9 +14,35 @@ module Rea
         board.on_board? @coord_x, @coord_y
       end
 
+      def neighbour direction
+        self.class.new neighbour_x(direction), neighbour_y(direction)
+      end
+
       protected
 
         attr_reader :coord_x, :coord_y
+
+      private
+      DELTA_X = {
+        'north' => 0,
+        'east' => 1,
+        'south' => 0,
+        'west' => -1
+      }
+      DELTA_Y = {
+        'north' => 1,
+        'east' => 0,
+        'south' => -1,
+        'west' => 0
+      }
+
+      def neighbour_x direction
+        @coord_x + DELTA_X[direction]
+      end
+
+      def neighbour_y direction
+        @coord_y + DELTA_Y[direction]
+      end
 
     end
   end

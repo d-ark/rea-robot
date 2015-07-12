1e0b31076a0d0ce6c0335333a824d932cd17f965
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 02:05:19 +0300

    [GREEN] Implemented Direction class

--- lib/rea/robot/direction.rb
+++ lib/rea/robot/direction.rb
@@ -1,6 +1,38 @@
 module Rea
   module Robot
     class Direction
+      def initialize direction
+        @direction = direction
+      end
+
+      def == other
+        self.direction == other.direction
+      end
+
+      def rotate_right
+        self.class.new next_direction
+      end
+
+      def rotate_left
+        self.class.new prev_direction
+      end
+
+      protected
+        attr_reader :direction
+
+      private
+
+        DIRECTIONS = ['north', 'east', 'south', 'west']
+
+        def next_direction
+          DIRECTIONS[ ( DIRECTIONS.find_index(direction) + 1 ) % DIRECTIONS.size]
+        end
+
+        def prev_direction
+          DIRECTIONS[ ( DIRECTIONS.find_index(direction) - 1 ) % DIRECTIONS.size]
+        end
+
+
     end
   end
 end

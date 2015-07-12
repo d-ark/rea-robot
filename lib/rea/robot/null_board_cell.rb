1842627b2cd884d6700b98f3f1e08fea693bab73
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 01:19:18 +0300

    [RED] Added null_board_cell (for invalid cells)

---
+++ lib/rea/robot/null_board_cell.rb
@@ -0,0 +1,6 @@
+module Rea
+  module Robot
+    class NullBoardCell
+    end
+  end
+end
---
+++ spec/rea/robot/null_board_cell_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+require "rea/robot/null_board_cell"
+
+describe Rea::Robot::NullBoardCell do
+  let(:board_cell) { described_class.new }
+
+  it 'null cell is not equal to any other' do
+    cell1 = described_class.new
+    cell2 = described_class.new
+    cell3 = Rea::Robot::BoardCell.new(2,2)
+
+    expect(cell1).not_to eq cell2
+    expect(cell1).not_to eq cell3
+  end
+
+  it 'is always outside board' do
+    expect(board_cell.on_board? double).not_to be
+  end
+
+
+  it 'represents cell' do
+    expect(board_cell.to_s).to eq ''
+  end
+
+  it 'finds neighbour' do
+    expect(board_cell.neighbour('north').class).to eq described_class
+  end
+
+end

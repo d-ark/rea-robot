546de0f1143f0198a2187a613d6840409fdb1edf
Anton Priadko <antonpriadko@gmail.com>
2015-07-12 19:42:58 +0300

    [RED] Added specs for #neighbour for board_cell

--- spec/rea/robot/board_cell_spec.rb
+++ spec/rea/robot/board_cell_spec.rb
@@ -26,6 +26,41 @@ describe Rea::Robot::BoardCell do
 
       board_cell.on_board? board
     end
+
   end
 
+  describe '#neighbour' do
+    # The origin (0,0) can be considered to be the SOUTH WEST most corner.
+    #
+    # -------  0 - (0,0)-point
+    # |     |  * - center point
+    # |  N  |  N - north point (1 step from center in north direction)
+    # | W*E |  E - east point (1 step from center in east direction)
+    # |  S  |  S - south point (1 step from center in south direction)
+    # |0    |  W - west point (1 step from center in west direction)
+    # -------
+
+    let(:center_cell) { described_class.new(2,2) }
+    let(:north_cell)  { described_class.new(2,3) }
+    let(:east_cell)   { described_class.new(3,2) }
+    let(:south_cell)  { described_class.new(2,1) }
+    let(:west_cell)   { described_class.new(1,2) }
+
+    it 'finds north neighbour' do
+      expect(center_cell.neighbour('north')).to eq north_cell
+    end
+
+    it 'finds east neighbour' do
+      expect(center_cell.neighbour('east')).to eq east_cell
+    end
+
+    it 'finds south neighbour' do
+      expect(center_cell.neighbour('south')).to eq south_cell
+    end
+
+    it 'finds west neighbour' do
+      expect(center_cell.neighbour('west')).to eq west_cell
+    end
+
+  end
 end

f6a14d61d227cea12a0c6575eebfe7d378491453
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 14:28:26 +0300

    [GREEN] Added MoveCommand

--- lib/rea/robot/commands/move_command.rb
+++ lib/rea/robot/commands/move_command.rb
@@ -1,6 +1,14 @@
 module Rea
   module Robot
     class MoveCommand
+      def initialize robot, _
+        @robot = robot
+      end
+
+      def call
+        @robot.move
+        ''
+      end
     end
   end
 end

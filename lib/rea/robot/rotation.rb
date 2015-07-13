b8b6302f12f29273f5b50683329b3d829b66a9bf
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 04:04:37 +0300

    [GREEN] Added Rotation

--- lib/rea/robot/rotation.rb
+++ lib/rea/robot/rotation.rb
@@ -1,6 +1,23 @@
+require 'rea/robot/left_rotation'
+require 'rea/robot/right_rotation'
+require 'rea/robot/null_rotation'
+
 module Rea
   module Robot
     class Rotation
+
+      def self.build(side)
+        STRATEGIES[side].new
+      end
+
+    private
+
+      STRATEGIES = {
+        'left' => LeftRotation,
+        'right' => RightRotation
+      }
+      STRATEGIES.default = NullRotation
+
     end
   end
 end

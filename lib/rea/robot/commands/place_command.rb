6ec699131dfe72c6324a22503e5e8cfe01908350
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 15:06:04 +0300

    [GREEN] Added PlaceCommand

--- lib/rea/robot/commands/place_command.rb
+++ lib/rea/robot/commands/place_command.rb
@@ -1,6 +1,23 @@
 module Rea
   module Robot
     class PlaceCommand
+      def initialize robot, args
+        @robot = robot
+        parse_argument Array(args).first.to_s || ''
+      end
+
+      def call
+        @robot.place @cell, @direction
+        ''
+      end
+
+      private
+
+      def parse_argument str
+        left,right,direction = str.split ','
+        @cell = BoardCellFactory.build left,right
+        @direction = DirectionFactory.build direction
+      end
     end
   end
 end

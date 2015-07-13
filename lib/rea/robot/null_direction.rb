e4c907b13eb2cc7a40b4f533c8b995a03342741b
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 03:11:00 +0300

    [GREEN] Added NullDirection

--- lib/rea/robot/null_direction.rb
+++ lib/rea/robot/null_direction.rb
@@ -1,6 +1,32 @@
+require 'rea/robot/board_cell_factory'
+
 module Rea
   module Robot
     class NullDirection
+      def == _
+        false
+      end
+
+      def valid?
+        false
+      end
+
+      def rotate_left
+        self.class.new
+      end
+
+      def rotate_right
+        self.class.new
+      end
+
+      def to_s
+        ''
+      end
+
+      def move _
+        BoardCellFactory.build nil, nil
+      end
+
     end
   end
 end

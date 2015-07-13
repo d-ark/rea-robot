d8338623104b1d78fbd6ff7057711087b7ff416a
Anton Priadko <antonpriadko@gmail.com>
2015-07-13 14:31:25 +0300

    [GREEN] Implemented ReportCommand

--- lib/rea/robot/commands/report_command.rb
+++ lib/rea/robot/commands/report_command.rb
@@ -1,6 +1,13 @@
 module Rea
   module Robot
     class ReportCommand
+      def initialize robot, _
+        @robot = robot
+      end
+
+      def call
+        @robot.position
+      end
     end
   end
 end

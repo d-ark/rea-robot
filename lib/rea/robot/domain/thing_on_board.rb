458f05eae6a18bc2e551026391cd3e88a7bc5532
Anton Priadko <anton.pr@randrmusic.com>
2015-07-16 10:00:04 +0300

    [GREEN] Implemented thing_on_board

--- lib/rea/robot/domain/thing_on_board.rb
+++ lib/rea/robot/domain/thing_on_board.rb
@@ -1,6 +1,14 @@
 module Rea
   module Robot
     class ThingOnBoard
+      def initialize thing, cell
+        @thing = thing
+        @cell = cell
+      end
+
+      private
+
+        attr_reader :thing, :cell
     end
   end
 end

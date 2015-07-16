e23544dcf49cca423016a652715f4e5d756f7d81
Anton Priadko <anton.pr@randrmusic.com>
2015-07-16 10:25:50 +0300

    [RED] Added on_cell? method spec for thing_on_board

--- spec/rea/robot/domain/thing_on_board_spec.rb
+++ spec/rea/robot/domain/thing_on_board_spec.rb
@@ -10,6 +10,13 @@ module Rea
       it 'creates thing_on_board' do
         expect(thing_on_board).to be
       end
+
+      describe '#on_cell?' do
+        it 'knows if he is on certan cell' do
+          expect(thing_on_board.on_cell? cell).to be
+          expect(thing_on_board.on_cell? BoardCellFactory.new 1, 1).not_to be
+        end
+      end
     end
   end
 end
